1d6ed718eaa5ddfce2a1a7766f629cdab0ddb910
brycekoltermann <bryce.koltermann@gmail.com>
2021-08-09 09:58:17 -0400

    Koltermann week 3 homework

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-const deckCards = ["Agility.png", "Agility.png", "Boat.png", "Boat.png", "Citizenship.png", "Citizenship.png", "Hack.png", "Hack.png", "Nerd-Rage.png", "Nerd-Rage.png", "Nuka-Cola.png", "Nuka-Cola.png", "Robotics.png", "Robotics.png", "Shock.png", "Shock.png"];
+const deckCards = ["Blue Hair Minecraft lady.png", "Blue Hair Minecraft lady.png", "Disco Minecraft.png", "Disco Minecraft.png", "Green Minecraft with sword.png", "Green Minecraft with sword.png", "minecraft Blue male.png", "minecraft Blue male.png", "Minecraft Pig.png", "Minecraft Pig.png", "Minecraft Skeleton.png", "Minecraft Skeleton.png", "Minecraft Zombie.png", "Minecraft Zombie.png", "Purple Minecraft with crossbow.png", "Purple Minecraft with crossbow.png"];
 
 const deck = document.querySelector(".deck");
 let opened = [];
@@ -9,6 +9,7 @@ const playAgain = document.querySelector(".play-again-btn");
 const movesCount = document.querySelector(".moves-counter");
 let moves = 0;
 const star = document.getElementById("star-rating").querySelectorAll(".star");
+console.log ([star])
 let starCount = 3;
 const timeCounter = document.querySelector(".timer");
 let time;
@@ -33,25 +34,25 @@ function shuffle(array) {
 
 // TODO: Implement this function
 function startGame() {
-  // Invoke shuffle function and store in variable
+  // Invoke shuffle function and store in variable. This creates a new array of random array and runs for the duration of the loop below (defined by the current const=deckCards). The loop also creates a table data element and makes it a child (.deck).This is used to build the display. and assigns the shuffled images to a child of the table data (.card)
   const shuffledDeck = shuffle(deckCards);
   // Implement a for loop on the shuffledDeck array
-  
-    // Create the <td> tags and assign it to a variable called tdTag
-    
+  for (let i = 0; i < shuffledDeck.length; i++) {
+     // Create the <td> tags and assign it to a variable called tdTag
+    const tdTag = document.createElement('td');
     // Give tdTag Element a class of card
-    
+    tdTag.classList.add('card');
     // Create the <img> tag and assign it to an addImage variable
-    
+    const addImage = document.createElement('img');
     // make the addImage a child of the tdTag
-    
-    // Set the addImage element src path with the shuffled deck
-    // TODO: replace the REPLACE ME string with the element in the shuffledDeck array at index i
-    addImage.setAttribute('src', 'img/' + 'REPLACE ME with the element in shuffleDeck at index i');
+    tdTag.appendChild(addImage);
+    // Set the addImage element src path with the shuffled deck and replace the REPLACE ME string with the element in the shuffledDeck array at index i
+    addImage.setAttribute('src', 'img/' + shuffledDeck[i]);
     // Add an alt tag to the addImage element
     addImage.setAttribute('alt', 'image of vault boy from fallout');
     // make the tdTag element a child of the deck element
-    
+    deck.appendChild(tdTag);
+  }
 }
 
 startGame();
@@ -111,10 +112,11 @@ function incrMovesCounter() {
 }
 
 function adjustStarRating() {
-  if (moves === 14) {
+   if (moves === 14) {
     // First element child is the <i> within the <li>
     star[2].firstElementChild.classList.remove("fa-star");
     starCount--;
+    console.log (starCount)
   }
   if (moves === 18) {
     star[1].firstElementChild.classList.remove("fa-star");
@@ -122,47 +124,48 @@ function adjustStarRating() {
   }
 }
 
-
-// TODO: stub out this function after the compare the two images src comment
 function compareTwo() {
-  // When there are 2 cards in the opened array
+  // This function is true only if 2 cards are opened in the array opened[]. If this i true it sets pointer events to none (does not allow more clicks) and then compares the cards using the logic if opened array = 2 and the string.src are equal then it calls the function displayMatchingCards() and console logs "its a match". If the card string.src != then it calls the function displayNotMatchingCards() and console logs "No Match"
   if (opened.length === 2) {
     // Disable any further mouse clicks on other cards
     document.body.style.pointerEvents = "none";
+    // Compare the two images src in the opened array
+    //implement
+    // if the opened array has a length of two && the element at index = 0 src string
+    // equals the element at index 1 src string
+    // the image srcs match
+    if (opened.length ===2 && opened[0].src === opened[1].src) {
+      //Invoke the displayMatchingCards()
+      displayMatchingCards();
+      // TODO: console log "It's a Match!" 
+      console.log ("It's a Match!");
+    } 
+    // TODO: if the image src's do not match
+    else if (opened.length ===2 && opened[0].src !== opened[1].src) {
+       // invoke the displayNotMatchingCards()
+      displayNotMatchingCards();
+      //console log "No Match!"
+      console.log ("No Match!");
+    }
   }
-  // Compare the two images src in the opened array
-  // TODO: implement
-  // if the opened array has a length of two && the element at index = 0 src string
-  // equals the element at index 1 src string
-  // the image srcs match
-  
-    // TODO: Invoke the displayMatchingCards()
-    // TODO: console log "It's a Match!"  
-    
-    
-  // TODO: if the image src's do not match
-  
-    // TODO: invoke the displayNotMatchingCards()
-    // TODO: console log "No Match!"
-  
 }
 
-// TODO:
+
 function displayMatchingCards() {
-  /* Access the two cards in opened array and add
-  the class of match to the imgages parent: the <li> tag
-  */
+  //When this function is called it uses the setTimeout(function()) to add a class of (.match) to the opened[] objects. It then pushes the objects with class (.match) to the matched[] and calls the checkIsGameFinished function and clears the opened[]. It then calls the functions incrMovesCounter and adjustStar Rating.
+
   setTimeout(function() {
-    // add the match class (Why are we adding it to the parentElement?)
+    // add the match class (We adding the match class to the parentElement so that the imgs remain visible and can be pushed to a new array called matched)
       // the match class should make the img visible
     opened[0].parentElement.classList.add("match");
     opened[1].parentElement.classList.add("match");
     // TODO: Push the flipped cards (opened[0] and opened[1]) to the matched array
-    
+    matched.push(opened[0]);
+    matched.push(opened[1]);
     // Allow for further mouse clicks on cards
     document.body.style.pointerEvents = "auto";
     // TODO: invoke the checkIsGameFinished function
-    
+    checkIsGameFinished();
    
     // Clear the opened array
     opened = [];
@@ -190,43 +193,41 @@ function displayNotMatchingCards() {
 }
 
 function addStatsToModal() {
-  // Access the modal content div
+  // sets a const statsParent to acces the div.modal-content and uses a loop to create 3 paragraphs and add a class(.stats) to them and assigns the p.stats as a child to the div.modal-content. The new Element with class Stats is then added to an array and uses the inner HTML with the array position to include text with number of moves, total time and starcount. The var moves, minutes and starCount are used to display the current numbers.
   const statsParent = document.querySelector(".modal-content");
   // Create three different paragraphs
   for (let i = 1; i <= 3; i++) {
     // Create a new Paragraph
-    // TODO: create p tag and assign it a newly created statsElement variable
-    
+    // create p tag and assign it a newly created statsElement variable
+    const statsElement = document.createElement('p');
     // Add a class to the new Paragraph
-    // TODO: add the stats class to the statsElement
-    
-    
+    // add the stats class to the statsElement
+    statsElement.classList.add('stats');
     // Add the new created <p> tag to the modal content
-    // TODO: add the statsElement as a child of the statsParent element
-    
+    // add the statsElement as a child of the statsParent element
+    statsParent.appendChild(statsElement);
   }
   // Select all p tags with the class of stats and update the content
   let p = statsParent.querySelectorAll("p.stats");
   // Set the new <p> to have the content of stats (time, moves and star rating)
   // TODO: Update all of the innerHTML text appropriately
-  p[0].innerHTML = "Update the time here with the minutes and seconds";
-  p[1].innerHTML = "Update this with how many moves it took";
-  p[2].innerHTML = "Update this with the star rating";
+  p[0].innerHTML = "You completed the game in " + moves + " moves";
+  p[1].innerHTML = "With a total time of: " + minutes + " min " + seconds + " sec";
+  p[2].innerHTML = "And earned " + starCount + " out of 3 stars";
 }
 
-// TODO: Implement the pseudocode
+//Implement the pseudocode
 function displayModal() {
-// use getElementByID to grab the id="close" element and assign it to a variable called modalClose
-
+// assigns elements modalClose and modal by using the getElementByID to grab the id="close" and id="modal". The modal.style is used to change the display from "none" to "block", this allows the modal to appear. the modalClose uses the event handler on click to set the display to "none".
+const modalClose = document.getElementById('close');
 // use getElementByID to grab the id="modal" element and assign it to a variable called modal
-
-// Set modal to display block to show it
-
-
+const modal = document.getElementById('modal');
+// Set modal to display block to show it  
+modal.style.display = "block";
 // When the user clicks on the modalClose <span> (x), 
 modalClose.onclick = function() {
     // set modal to diplay none
-    
+    modal.style.display = "none";
 };
 // When the user clicks anywhere outside of the modal, close it
   window.onclick = function(event) {
@@ -239,27 +240,22 @@ modalClose.onclick = function() {
 }
 
 function checkIsGameFinished() {
-  // there are 8 images total
-  // if the matched array has 16 elements,
+  // this functions checks how many objects are in the matched[]. If there are 16 objects the stopTime, addStatsToModal, and displayModal functions are called.
   if (matched.length === 16) {
     // stop the game
-    //TODO: invoke the stopTime function
-    
+    //invoke the stopTime function
+    stopTime();
     // tally stats
     // TODO: invoke the addStatsToModal
-    
+    addStatsToModal();
     
     // display modal
-    // TODO: invoke the displayModal function
-    
+    // invoke the displayModal function
+    displayModal();
     
   }
 }
 
-// if a card is clicked
-  // if timerStart is false
-      // start timer
-  // flip the card
 
 deck.addEventListener("click", function(evt) {
   if (evt.target.nodeName === "TD") {
